9f8deb6ccf3d1c8204f1482d49ded97df8ea3fd4
KaikaiTheUnderDog <121786928+KaikaiTheUnderDog@users.noreply.github.com>
2025-06-04 08:18:28 +0700

    Update cartModel.ts
    Fixed an issue with multiple items removal in cart
    changed from remove single items in a loop to passing a whole line items to removeCartItems and remove them from there

--- src/models/cartModel.ts
+++ src/models/cartModel.ts
@@ -12,9 +12,8 @@ interface ICart extends Document {
   user: Types.ObjectId;
   lineItems: ICartProduct[];
   subtotal: number;
-  removeCartItem: (
-    productId: Types.ObjectId,
-    variantId: Types.ObjectId
+  removeCartItems: (
+    items: { product: Types.ObjectId; variant: Types.ObjectId }[]
   ) => Promise<void>;
 }
 
@@ -46,21 +45,23 @@ const cartSchema = new Schema<ICart>({
   },
 });
 
-cartSchema.methods.removeCartItem = async function (
-  productId: Types.ObjectId,
-  variantId: Types.ObjectId
+cartSchema.methods.removeCartItems = async function (
+  items: { product: Types.ObjectId; variant: Types.ObjectId }[]
 ) {
-  const itemIndex = this.lineItems.findIndex(
-    (item: ICartProduct) =>
-      item.product.toString() === productId.toString() &&
-      item.variant.toString() === variantId.toString()
-  );
-
-  if (itemIndex > -1) {
-    this.lineItems.splice(itemIndex, 1);
-    await this.save();
+  for (const { product, variant } of items) {
+    const index = this.lineItems.findIndex(
+      (item: ICartProduct) =>
+        item.product.toString() === product.toString() &&
+        item.variant.toString() === variant.toString()
+    );
+    if (index > -1) {
+      this.lineItems.splice(index, 1);
+    }
   }
+
+  await this.save();
 };
+
 
 cartSchema.pre<ICart>("save", async function (next) {
   await this.populate("lineItems.product"); // Populate the variant field
